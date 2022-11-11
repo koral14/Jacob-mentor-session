9280be0229d76bac0f8c75569a8ff3f3fa407529
koral14 <105457134+koral14@users.noreply.github.com>
2022-11-10 16:44:34 -0800

    modified App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import React, { useState } from "react";
-import ListItem from "./list-item";
-
-const pets = ["dog", "cat", "fish"];
+import React from 'react';
+import SearchPage from './search-page/search-page';
 
 function App() {
-    // useState returns an array [stateVariable, setterFunction]
-    const [selectedPet, setSelectedPet] = useState('Gerbil');
-    const inputHandler = (event) => {
-      console.log(event.target.value);
-    }
-    const handlePetChange = (newPet) => {
-      setSelectedPet(newPet);
-    }
-    console.log('rendering')
-    return (
-        <div style={{ textAlign: "center" }}>
-            <div>Selected Pet: {selectedPet}</div>
-            <input
-                type="number"
-                onChange={inputHandler}
-            />
-            {pets.map((pet) => {
-                return <ListItem title={pet} name="Bubbles" onButtonClick={handlePetChange}/>;
-            })}
-        </div>
-    );
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <SearchPage />
+    </div>
+  );
 }
 
 export default App;
