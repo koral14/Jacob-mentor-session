bf757ebaa186e4e3cde70769d1d836d0fd09a0af
koral14 <105457134+koral14@users.noreply.github.com>
2022-11-29 11:01:46 -0800

    correction

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import StateTutorial from './StateTutorials';
 import Select from './Select';
 import InputField from './inputField';
 
 function App() {
-  
+  const [catImg, setCatImg] = useState('');
+
+  useEffect(() => {
+    fetch('https://api.thecatapi.com/v1/images/search')
+      .then(res => res.json())
+      .then(data => setCatImg(data[0].url))
+  }, [catImg])
 
   return (
     <>
-      <StateTutorial />
-      <Select />
-
-      <InputField />
-
+      <h1>Cat Photo</h1>
+      <img src={catImg} alt="Kitty" />
     </>
   );
 }
