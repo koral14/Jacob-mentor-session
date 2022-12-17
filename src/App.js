943837be0fa1ee8189aeb1c4a60913bbce0ce347
koral14 <105457134+koral14@users.noreply.github.com>
2022-12-17 13:15:22 -0800

    new

--- src/App.js
+++ src/App.js
@@ -1,70 +1,127 @@
-import React, {useState, useEffect} from 'react';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
+import React, { useReducer, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const initialList = [
+  {
+    id: 'a',
+    name: 'Robin',
+  },
+  {
+    id: 'b',
+    name: 'Dennis',
+  },
+]
+
+const initFamName = [
+  {
+    id: 1,
+    famName: "Gorkii",
+  },
+  {
+    id: 2,
+    famName: "Zombi",
+  }
+]
+
+// 3rd version of adding using useReducer:
+const listReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_ITEM':
+      return state.concat({ name: action.name, id: action.id });
+    default:
+      throw new Error();
+  }
+};
 
 function App() {
+  const [list, dispatchList] = useReducer(listReducer, initialList);
+  const [name, setName] = useState('');
 
-  // const [todoList, setTodoList] = useState([]);
-  // const [isLoading, setIsLoading] = useState(true);
-
-  const [todoListProp, setTodoList] = useState({
-    todoList: [],
-    isLoading: true,
-  });
-  const {todoList, isLoading} = todoListProp;
-
-  useEffect(() => {
-    new Promise ((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          data: {
-            todoList: JSON.parse(localStorage.getItem('savedTodoList')) || [],
-          },
-        });
-      }, 2000);
-    // }).then((result) => {
-    //   setTodoList([...result.data.todoList]);
-    //   setIsLoading(false);
-    }).then((result) => {
-
-      setTodoList({todoList: result.data.todoList, isLoading: false})
-      console.log(result)
-  });
-    // });
-  }, []);
-
-  useEffect(() => {
-    if (!isLoading) {
-      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-    }
-    }, [todoList, isLoading]);
-  
-
-  const addTodo = (newTodo) => {
-    setTodoList({todoList:[...todoList, newTodo]});  
+  const handleChange = (e) => {
+    setName(e.target.value);
   }
 
-  const removeTodo = (item) => {
-    const modifiedTodo = todoList.filter(
-      (todo) => item.id !== todo.id
-    );
-    setTodoList({todoList:modifiedTodo});
-  };
- 
+  const handleAdd = () => {
+    dispatchList({ type: 'ADD_ITEM', name, id: uuidv4() });
+    setName('');
+  }
+
+  // const [list, setList] = useState(initialList);
+  // const [name, setName] = useState('');
+
+  const [listFam, setListFam] = useState(initFamName);
+  const [famName, setFamName] = useState('');
+
+  // const handleChange = (e) => {
+  //   // track input field's state
+  //   setName(e.target.value);
+  // }
+  
+  // const handleAdd = () => {
+  //   // add item: const array3 = array1.concat(array2); 
+  //   // object shorthand initialization- variable's name equals the object's property name { name }; how to write it differently if not using shorthand...?
+  //   const newList = list.concat({ name, id: uuidv4() });
+  //   setList(newList);
+  //   setName('')
+  // }
+
+  const handleChangeFamName = (e) => {
+    setFamName(e.target.value);
+  }
+
+  const handleAddFamName = () => {
+    // const newListFam = listFam.concat({ listFam, id: uuidv4() });
+    // setListFam(newListFam);
+    // setFamName('');    
+    const newListFam = listFam.concat({ famName, id: uuidv4() });
+    setListFam(newListFam);
+    setFamName('')
+  }
+
   return (
     <div>
-      <h1>ToDo List</h1>
+      {/* the same but with smaller components and props for 2nd and 3rd */}
+      <AddItem 
+        name={name}
+        handleChange={handleChange}
+        handleAdd={handleAdd}
+      />
+      <List list={list} />
+      
+      <hr />
+      {/* the same but with all code in return statement */}
+      <div>
+        <input type="text" onChange={handleChangeFamName} value={famName} placeholder='Family name...'  />
+        <button type="button" onClick={handleAddFamName}>
+          Add Family Name
+        </button>
+      </div>
 
-      <AddTodoForm onAddTodo={addTodo} />
-
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
-      )
-      }
+      <hr />
+      <ol>
+        {listFam.map((item) => (
+          <li key={item.id}>{item.famName}</li>
+        ))}
+      </ol>
     </div>
   );
-}
+};
+
+const AddItem = ({ name, handleAdd, handleChange }) => (
+  <div>
+    <input type='text' onChange={handleChange} value={name} placeholder="name..." />
+    <button type="button" onClick={handleAdd}>
+      Add
+    </button>
+    </div>
+)
+
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.id}>{item.name}</li>
+    ))}
+  </ul>
+)
 
 export default App;
