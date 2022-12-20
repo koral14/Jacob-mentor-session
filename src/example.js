386a93d8518798a65fdf5daf85154612b7ca6ff1
koral14 <105457134+koral14@users.noreply.github.com>
2022-12-20 10:26:07 -0800

    count

---
+++ src/example.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+function Example() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        // update the document title using the browser API
+        document.title = `You clicked ${count} times`;
+    });
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)} >
+                Click me
+            </button>
+        </div>
+    );
+}
+
+export default Example;
