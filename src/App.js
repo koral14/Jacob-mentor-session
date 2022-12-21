662432b1dc79f0755785c50182da35fe7363a899
koral14 <105457134+koral14@users.noreply.github.com>
2022-12-21 11:51:28 -0800

    b

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React, { useState, useEffect } from 'react';
+// import JokeCard from './jokeCard';
 
+const loadJokes = async () => {
+  const response = await fetch('https://swapi.dev/api/people', {
+    headers: {"Content-Type": "application/json"},
+  });
+  const data = await response.json();
+  if (data) {
+    return data.results;
+  }
+  return [];
+}
 
 function App() {
 
-  return (
+  const [jokes, setJokes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    if (!jokes) {
+      loadJokes()
+        .then((data) => setJokes(data))
+        .then(() => setIsLoading(false))
+        .catch((error) => console.log(error.message));
+    }
+  }, [jokes]);
+
+  console.log(jokes);
+
+  return (
+    <div>
+      <h1>Hello API</h1>
+        {/* {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          jokes.map((joke) => 
+            <div key={joke.id}>{joke.setup}<div/>)
+        )} */}
+        
+    </div>
   )
 }
 
---
+++ src/jokeCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const JokeCard = (prop) => {
+    return (
+        <li>{prop.id}</li>
+    )
+}
+
+export default JokeCard;
