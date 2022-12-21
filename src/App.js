c5c6301e055b41f2929db1952cf58a1d54899b6d
koral14 <105457134+koral14@users.noreply.github.com>
2022-12-21 12:20:22 -0800

    m

--- src/App.js
+++ src/App.js
@@ -2,35 +2,17 @@ import React, { useState, useEffect } from 'react';
 // import JokeCard from './jokeCard';
 
 const loadJokes = async () => {
-  const response = await fetch(`https://swapi.dev/api/people`, {
-    headers: { "Content-Type": "application/json" },
-  });
+  const response = await fetch('https://catfact.ninja/fact');
   const data = await response.json();
   if (data) {
-    return data.results;
+    return data;
 }
   return [];
 }
-
-const LoadPeople = async() => {
-  // get the response
-  const response = await fetch(`https://swapi.dev/api/people`, {
-      // this tells the API that you want your data in JSON format
-      headers: { "Content-Type": "application/json" },
-  });
-  // pull out your data
-  const data = await response.json();
-  // if there's data, return the results (where te people are)
-  if (data) {
-      return data.results;
-  }
-  // if there's null data, return an empty array
-  return [];
-};
 
 function App() {
 
-  const [jokes, setJokes] = useState([]);
+  const [jokes, setJokes] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -47,15 +29,16 @@ function App() {
   return (
     <div>
       <h1>Hello API</h1>
-        {/* {isLoading ? (
+
+        {isLoading ? (
           <div>Loading...</div>
         ) : (
-          jokes.map((joke) => 
-            <div key={joke.id}>{joke.setup}<div/>)
-        )} */}
+          jokes.map((joke) => <div key={joke.fact}>{joke.length}</div>)
+        )}
         
     </div>
   )
 }
 
 export default App;
+
---
+++ src/test.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const loadPeople = async () => {
+  const response = await fetch(`https://swapi.dev/api/people`, {
+    headers: { "Content-Type": "application/json" },
+  });
+  const data = await response.json();
+  if (data) {
+    return data.results;
+  }
+  return [];
+};
+
+function App() {
+  // Store people and status of fetching people data
+  const [people, setPeople] = useState();
+  const [isLoading, setIsLoading] = useState(true)
+  
+  useEffect(() => {
+    // If people is undefined or null, call load people to get them from the API
+    if (!people) {
+      loadPeople()
+        // Once the promise is resolved, set people equal to the data response
+        .then((data) => setPeople(data))
+        .then(() => setIsLoading(false))
+        // If there's an error and it isn't successfully resolved, store that
+        .catch((error) => console.log(error.message));
+    }
+  }, [people]);
+
+  // console.log(people);
+  // If people is not loading or errored, iterate through and display the individual's names
+  return (
+    <div className="App">
+      <header className="App-header">
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          people.map((person) => <div key={person.name}>{person.name}</div>)
+        )}
+      </header>
+    </div>
+  );
+}
+// export default App;
