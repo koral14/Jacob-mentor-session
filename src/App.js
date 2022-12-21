22186dbb497abe878bdb660db841606588cd8fcb
koral14 <105457134+koral14@users.noreply.github.com>
2022-12-21 11:55:05 -0800

    n

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React, { useState, useEffect } from 'react';
 // import JokeCard from './jokeCard';
 
 const loadJokes = async () => {
-  const response = await fetch('https://swapi.dev/api/people', {
-    headers: {"Content-Type": "application/json"},
+  const response = await fetch(`https://swapi.dev/api/people`, {
+    headers: { "Content-Type": "application/json" },
   });
   const data = await response.json();
   if (data) {
     return data.results;
-  }
+}
   return [];
 }
+
+const LoadPeople = async() => {
+  // get the response
+  const response = await fetch(`https://swapi.dev/api/people`, {
+      // this tells the API that you want your data in JSON format
+      headers: { "Content-Type": "application/json" },
+  });
+  // pull out your data
+  const data = await response.json();
+  // if there's data, return the results (where te people are)
+  if (data) {
+      return data.results;
+  }
+  // if there's null data, return an empty array
+  return [];
+};
 
 function App() {
 
